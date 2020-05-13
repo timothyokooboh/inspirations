bc757d8deacd156dda1215efea453b1ff577a8cd
Timothy Okooboh <okoobohtimothy@gmail.com>
2020-05-13 20:41:08 +0100

    edited the offline page

--- resources/views/vendor/laravelpwa/offline.blade.php
+++ resources/views/vendor/laravelpwa/offline.blade.php
@@ -1,7 +1,36 @@
 @extends('layouts.app')
 
+@section('styles')
+    <style>
+        main {
+            margin-top: 85px;
+        }
+
+        div {
+            margin-bottom: 20px;
+        }
+    </style>
+@endsection
+
 @section('content')
 
-    <h1>You are currently not connected to any networks.</h1>
+    <div class="container">
+        <div>
+            <h2>
+                I have a good news and bad news.
+            </h2>
+        </div>
+        <div>
+            <h4>
+                The bad news is that you are currently not connected to any network.
+            </h4>
+        </div>
+        <div>
+            <h4>
+                The good news is that YOU ARE AMAZING! <br>
+                Never forget that.
+            </h4>
+        </div>
+    </div>
 
 @endsection
